684ceba1b553c6ffa66b8f5e10f8f24c4d9ed669
vams88 <cvamshi88@gmail.com>
2026-09-09 22:54:08 +0530

    Delete admin/config.js

--- admin/config.js
+++
@@ -1,4 +0,0 @@
-export const SUPABASE_URL="https://junnutmwepwnixwqoxya.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY="sb_publishable_TfLyvGs1z4g76RRXz3webQ_fII1E9S0";
- export const GOOGLE_MAPS_API_KEY = "AIzaSyBCYbblVZvvQz1zj-6uecc5aObcOZbwIOc";
- 
