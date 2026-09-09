725040ca0dea861d4fa613c4ad88cbc7a43cc5fb
vams88 <cvamshi88@gmail.com>
2026-09-09 07:09:40 +0530

    Add files via upload

---
+++ driver-rider/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL="https://junnutmwepwnixwqoxya.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY="sb_publishable_TfLyvGs1z4g76RRXz3webQ_fII1E9S0";
+ export const GOOGLE_MAPS_API_KEY = "AIzaSyBCYbblVZvvQz1zj-6uecc5aObcOZbwIOc";
